aae2007396da14beec57b472fbeef48836dda0eb
Trevor Kimenye <kimenye@gmail.com>
2014-11-28 19:45:26 +0300

    Fixed footer

---
+++ footer.php
@@ -0,0 +1,29 @@
+<footer>
+  <div class="information">
+    <section>      
+      <div class="opening">
+        <h6>CHAKA RANCH LEISURE PARK</h6>
+        <p>Entry Fee:- Children 100, Adults 200<br />
+        Open Saturdays, Sundays &amp; Public Holidays <em>8AM - 6PM</em></br />
+        Weekdays:- Groups only, minimum 15pax by special arrangement.<br />
+        For enquiry write to <a href="mailto:info@chakaranch.com">info@chakaranch.com</a></p>
+      </div>
+      <div class="contacts">
+        <ul>
+          <li>Tel: <tel>0738600046 / 0719242897</tel></li>
+          <li><a href="javascript:void(0)">Facebook</a></li>
+          <li><a href="javascript:void(0)">Twitter</a></li>
+          <li><a href="javascript:void(0)">YouTube</a></li>
+          <li><a href="javascript:void(0)">Instagram</a></li>
+        </ul>
+      </div>      
+      <div class="bike">
+      </div>
+    </section>
+  </div>
+  <div class="copyright">
+    <section>
+      <article><p>Copyright &copy; 2014. Chaka Ranch. All rights reserved</p></article>
+    </section>
+  </div>      
+</footer>
---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Chaka Ranch | Take the driver's seat!</title>  
+    <link rel="stylesheet" href="assets/stylesheets/style.css" />
+  </head>
+ 	
+ 	<body>
+ 		<?php require 'footer.php';?>
+ 	</body>
+</html>
