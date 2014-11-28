7e64e95bed9bc118a5cfc3c896d38a826d9a7752
Trevor Kimenye <kimenye@gmail.com>
2014-11-28 20:47:56 +0300

    added main copy

--- index.php
+++ index.php
@@ -8,6 +8,22 @@
   </head>
  	
  	<body>
+ 		<section class="main">
+ 			<aside>
+ 				<h2><span class="light">Karibu</span> Chaka Ranch</h2>
+ 				<p>Chaka Ranch Leisure Park, an exciting new adrenaline filled getaway on the slopes of Mount Kenya. Stunning view of Mount Kenya, BBQs, food court, a bar and seating area with a beautiful dam view. You can take the driver's seat and race a variety of powerful off road buggies on a purpose built 2km race track.</p>
+ 				<p>Quad bike tours for the whole family, including a junior section where children from as young as six years old can also enjoy rides on quad bikes and buggies. Ideal for family outings, out of town friends outing and team building.</p>
+
+ 				<button>Explore...</button>
+ 			</aside>
+<!--  			<article>
+ 				<div class="video">
+				  <div class="video-wrapper">
+				      <iframe src="http://www.youtube.com/embed/ksVJHfxG46g?showinfo=0&iv_load_policy=3&controls=0" frameborder="0" allowfullscreen></iframe>
+				  </div>
+				</div>
+ 			</article> -->
+ 		</section>
  		<?php require 'footer.php';?>
  	</body>
 </html>
