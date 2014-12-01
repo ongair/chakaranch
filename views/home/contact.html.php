c56610a2ec23f269277c4d67c6256a608f8d985e
Trevor Kimenye <kimenye@gmail.com>
2014-12-01 19:39:47 +0300

    updated directions

--- views/home/contact.html.php
+++ views/home/contact.html.php
@@ -2,7 +2,18 @@
 </div>
 <section class="main">
 	<div class="map">
+		<h2><span class="light">Getting Here</span></h2>
+		<p>Chaka Ranch is located off the Nyeri-Nanyuki highway. Below is a map of how to get to there.</p>
+		<img src="assets/images/directions.jpg" />
 	</div>
-	<form method="POST" action="contact">
-	</form>
+	<div class="form">
+		<h2><span class="light">Contact Us</span></h2>
+		<form method="POST" action="contact">			
+			<input type="text" name="name" placeholder="Your Name" />
+			<input type="email" name="email" placeholder="Your Email" />
+			<input type="tel" name="mobile" placeholder="Your Mobile" />
+			<textarea name="comment" placeholder="Your message"></textarea>
+			<button>SEND</button>
+		</form>
+	</div>
 </section>
